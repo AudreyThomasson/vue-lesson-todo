a406159c0342867a74dc9c507fccf628424025af
Audrey Thomasson <audrey@macbook-pro.attlocal.net>
2021-03-03 10:48:35 -0600

    starter files

---
+++ src/components/TodoList.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>Hello from the TodoList</h1>
+</template>
+
+<script>
+import { todoItems } from "../data";
+
+export default {
+    data() {
+        return {
+        todos: [...todoItems],
+        };
+    },
+}
+</script>
+
+<style>
+
+</style>
