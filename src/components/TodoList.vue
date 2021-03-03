c9c12a1c23046e126a7a402791ce81aaa6fcced0
Audrey Thomasson <audrey@macbook-pro.attlocal.net>
2021-03-03 11:55:56 -0600

    buttons work

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,17 +1,39 @@
 <template>
-  <h1>Hello from the TodoList</h1>
+  <div>
+    <h3>ToDo List</h3>
+    <h5>There are {{ itemsRemaining.length }} items left to do today</h5>
+    <div v-for="item in todos" :key="item.id">
+      <TodoItem :todo="item" @status-change="handleStatusChange"/>
+    </div>
+  </div>
 </template>
 
 <script>
 import { todoItems } from "../data";
+import TodoItem from "./TodoItem.vue";
 
 export default {
+    components: { TodoItem },
+
     data() {
         return {
-        todos: [...todoItems],
+            todos: [...todoItems],
         };
     },
-}
+
+    methods: {
+        handleStatusChange(item) {
+            item.complete = !item.complete;
+            console.log(item);
+        },
+    },
+    computed: {
+        itemsRemaining() {
+            return this.todos.filter((t) => !t.complete);
+        },
+    },
+};
+
 </script>
 
 <style>
